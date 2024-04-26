0546b757c2f155b6b1679ad01d207c8c28f832d0
Tadeo Junco <135076459+Tadeo-Junco@users.noreply.github.com>
2024-04-25 21:49:25 -0300

    Ejercicio Terminado

--- tpsProgramacion3/src/App.jsx
+++ tpsProgramacion3/src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
-
 import './App.css'
+import Table from './components/table/Table';
 
 function App() {
-  
+  const netIncomes = [
+    {brand: "McDonalds", income: 1291283},
+    {brand: "Burger King", income: 1927361}, 
+    {brand: "KFC", income: 1098463}
+  ];
 
   return (
     <>
-      
+      <Table data={netIncomes}/> 
     </>
   )
 }
